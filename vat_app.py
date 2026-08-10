81f412ec167ae10fd7e49accf3ec775f3fa10eae
403-35-supamas <56302@yupparaj.ac.th>
2026-08-10 09:00:20 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,9 @@
+import stramlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07
+net_price = price - vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.divider() 
+st.write("นางสาวศุภมาศ อยู่สิงห์ ม.4/3 เลขที่ 35") 
