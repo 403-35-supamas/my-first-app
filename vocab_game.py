6eb681a298d783c91bf8897f38d15ca1c29fa98b
403-35-supamas <56302@yupparaj.ac.th>
2026-08-17 13:17:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,6 +101,6 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางสาวศุภมาศ อยู่สิงห์ เลขที่ 35 ม.4/3")
 
 
