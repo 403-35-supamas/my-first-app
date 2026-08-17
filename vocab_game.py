5675be08e7f3e13265f1992552f71b3fabbb1d21
403-35-supamas <56302@yupparaj.ac.th>
2026-08-17 13:38:25 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,14 +52,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
      # ตรวจข้อ 3
-    if u_ans1 == "pineapple":
+    if u_ans3 == "pineapple":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans2 == "glasses":
+    if u_ans4 == "glasses":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
