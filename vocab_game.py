6dd456c09ed93bd40d5e1ce1c3ca6e2c5a566a96
403-35-supamas <56302@yupparaj.ac.th>
2026-08-17 13:36:29 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -128,7 +128,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาวศุภมาศ อยู่สิงห์ เลขที่ 35 ม.4/3")
